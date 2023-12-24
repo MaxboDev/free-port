1445f803b434322eed02032c4128ff798a98344f
Max Borkowski <max.borkowski+copilot@lexisnexisrisk.com>
2023-12-24 20:32:24 +0000

    updating versioning logic

---
+++ ci/getVersion.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get the latest tag
+git fetch --tags
+latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+# If there are no tags, start with 1.0.0
+if [[ -z "$latestTag" ]]; then
+  echo "::set-output name=VERSION::1.0.0"
+else
+  # If there are tags, increment the patch version
+  versionParts=(${latestTag//./ })
+  patchVersion=${versionParts[2]}
+  patchVersion=$((patchVersion+1))
+  newVersion="${versionParts[0]}.${versionParts[1]}.$patchVersion"
+  echo "::set-output name=VERSION::$newVersion"
+fi
