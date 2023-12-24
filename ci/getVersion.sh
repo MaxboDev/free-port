d1915e069ba2589e42c66f24fdf80cffd036dfbc
Max Borkowski <max.borkowski+copilot@lexisnexisrisk.com>
2023-12-24 23:07:21 +0000

    fixing versioning logic

--- ci/getVersion.sh
+++ ci/getVersion.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-# Get the latest tag
 git fetch --tags
 latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 
-echo latestTag: $latestTag
-
-# If there are no tags, start with 1.0.0
 if [[ -z "$latestTag" ]]; then
   echo "VERSION=1.0.0" >> $GITHUB_ENV
 else
-  # If there are tags, increment the patch version
-  versionParts=(${latestTag//./ })
-  patchVersion=${versionParts[2]}
-  patchVersion=$((patchVersion+1))
-  newVersion="${versionParts[0]}.${versionParts[1]}.$patchVersion"
-  echo "VERSION=$newVersion" >> $GITHUB_ENV
+  echo "VERSION=$latestTag" >> $GITHUB_ENV
 fi
