9b8c42e8c50a4883ce0806b3ae4fd3ecac728f26
Max Borkowski <max.borkowski+copilot@lexisnexisrisk.com>
2023-12-25 11:46:03 +0000

    fixing line endings in regex to work in windows

--- tests/test.js
+++ tests/test.js
@@ -69,7 +69,7 @@ describe('free-port CLI', () => {
     });
 
     await waitForExpect(() => {
-      expect(output).toMatch(/The following processes are running on port 1234:\n  👾 node: \d+\nWould you like to kill it\? \(Y\/n\) /);
+      expect(output).toMatch(/The following processes are running on port 1234:\r?\n  👾 node: \d+\r?\nWould you like to kill it\? \(Y\/n\) /);
     }, 1000);
 
     freePortProcess.stdin.write('\n'); // simulate user pressing enter
