262223bcb03303e3d2ede01cea8d616f6e2a6d10
Max Borkowski <max.borkowski+copilot@lexisnexisrisk.com>
2023-12-26 14:18:38 +0000

    removing unnecessary scripts

--- ci_scripts/getTestCommand.js
+++
@@ -1,9 +0,0 @@
-const os = require('os');
-
-if (os.platform() === 'win32') {
-  process.stdout.write('test:win');
-} else if (os.platform() === 'darwin') {
-  process.stdout.write('test:mac');
-} else {
-  process.stdout.write('test:nix');
-}
